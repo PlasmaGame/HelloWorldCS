d6216de1151782f11487e98608897d31348ed8a2
laimontr <128290019+TRSandwich@users.noreply.github.com>
2024-08-09 22:34:55 +0100

    Added Project
    Visual Studio 2017

---
+++ HelloWorldCS/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Program
+{
+	class HelloWorldCS
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World");
+			Console.Write("Press any key to continue");
+			Console.ReadKey();
+		}
+	}
+}
